ec33afc1d5b4a492a97e2305dc94499bdbb847cf
Pierrooow <97539842+Pierrooow@users.noreply.github.com>
2022-02-10 17:34:46 +0100

    lol

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# require 'as-duration'   #pbl d'install
 require 'faker'
+
+Doctor.destroy_all
+Patient.destroy_all
+Appointment.destroy_all
+
+
+# seed Spéciality
+my_speciality = ['angiologue','généraliste','cardiologue','dentiste','pneumologue']
+my_speciality.each { |spe| Speciality.create(name: spe) }
+
+# seed les Patient
+50.times do
+  Patient.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name
+  )
+end
+
+# seed les Doctor
+10.times do
+  s = Speciality.order("RANDOM()").limit(1).ids[0]
+  Doctor.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    speciality: s,
+    zip_code: Faker::Number.number(5)
+  )
+end
+
+# seed pour les City
+10.times do
+  City.create!(
+    name_town: Faker::Address.city
+  )
+end
 
